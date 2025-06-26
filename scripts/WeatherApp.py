50bd055fff3e1a2639e8ebfd4a1bdf7bf61b428e
Shun Kinoshita <shunthink@outlook.jp>
2025-06-26 12:43:20 +0900

    Script for WeatherApp using tkinter

---
+++ scripts/WeatherApp.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+class MainWindow(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        master.title("Weather App")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    mainWindow = MainWindow(master=root)
+
+    mainWindow.mainloop()
