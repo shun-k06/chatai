9ff40f09ab6ca6cb49352046f18e1f78cc5249e2
Shun Kinoshita <shunthink@outlook.jp>
2025-07-01 12:56:34 +0900

    Commit befores start learning with a cat book.

--- scripts/WeatherApp.py
+++ scripts/WeatherApp.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 
+
 class MainWindow(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         master.title("Weather App")
+
 
 if __name__ == "__main__":
     root = tk.Tk()
